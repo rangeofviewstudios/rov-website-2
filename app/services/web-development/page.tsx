f56b56f7ec66b457645a3e90510b726bd5fc3506
daksha11 <rongala.daksha11@gmail.com>
2025-12-17 14:51:09 +0530

    Fixed Error

--- app/services/web-development/page.tsx
+++ app/services/web-development/page.tsx
@@ -6,7 +6,7 @@ import DigitalStage from "../../../components/Web-Dev/DigitalStage";
 import WhatMakesUsDifferent from "../../../components/Web-Dev/WhatMakesUsDifferent";
 import HowWeWorkSection from "../../../components/Web-Dev/HowWeWorkSection";
 import HustleSection from "../../../components/Web-Dev/HustleSection";
-import ShowcaseSection from "../../../components/Web-Dev/ShowcaseSection.tsx";
+import ShowcaseSection from "../../../components/Web-Dev/ShowcaseSection";
 import WhoWeBuildFor from "../../../components/Web-Dev/WhoWeBuildFor";
 import FAQSection from "../../../components/Web-Dev/FAQSection";
 import BookACall from "../../../components/Web-Dev/BookACall";
